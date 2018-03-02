1c7a35e7d73c38af5a3449e176863620fd342ab2
Randercrop <austiwang@gmail.com>
2018-03-02 13:38:44 -0800

    Add files via upload

---
+++ Knight.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Knight
+    {
+        //using the array provided in 304
+        //int[] KTMOVi = new int[8] { -2, -1, 1, 2, 2, 1, -1, -2 };
+        //int[] KTMOVj = new int[8] { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+        //using the order given in the picture
+        //int[] KTMOVi = new int[8] { 1, 2, 2, 1, -1, -2, -2, -1 };
+        //int[] KTMOVj = new int[8] { -2, -1, 1, 2, 2, 1, -1, -2 };
+
+        //using the order in Deitel's Java how to Program
+        int[] KTMOVi = new int[8] {  2,  1, -1, -2, -2, -1, 1, 2};
+        int[] KTMOVj = new int[8] { -1, -2, -2, -1,  1,  2, 2, 1};
+        
+        //Holds how many movement options are avaiable from the current location
+        //A zero represents that location is outside the borders or has been previously traveled to
+        int[] mov1 = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        int loci;
+        int locj;
+        public int order;
+        Random rnd;
+
+        public Knight()
+        {
+            rnd = new Random();
+            loci = rnd.Next(0,8);
+            locj = rnd.Next(0,8);
+            order = 2;
+        }
+        public Knight(int i, int j)
+        {
+            rnd = new Random();
+            loci = i;
+            locj = j;
+            order = 2;
+        }
+        public bool reset(int i, int j){
+            loci = i;
+            locj = j;
+            order = 2;
+            return true;
+        }
+        public int Loci
+        {
+            get { return loci; }
+            set { loci = value; }
+        }
+        public int Locj
+        {
+            get { return locj; }
+            set { locj = value; }
+        }
+        //Checks the next 8 moves from the current location to see if it's out of bounds or has already been visited
+        //for all next valid spaces, we check the 8 moves from that space for how many moves from there are valid
+        public void checkmov(int[,] board)
+        {
+            for(int ind = 0; ind<8; ind++)
+            {
+                if(((loci+KTMOVi[ind])>=0) && ((loci + KTMOVi[ind])<8) && ((locj+KTMOVj[ind]) >=0) && ((locj + KTMOVj[ind]) <8) )
+                {
+                    if(board[(loci+KTMOVi[ind]),(locj+KTMOVj[ind])] == 0)
+                    {
+                        mov1[ind] = checknext(board, (loci + KTMOVi[ind]), (locj + KTMOVj[ind]))+1;
+                    }
+                }
+            }
+        }
+         
+        //checks the next iteration of 8 moves from the given position and returns how many moves from this position are valid
+        private int checknext(int[,] board, int ii, int jj)
+        {
+            int count = 0;
+            for (int next = 0; next < 8; next++)
+            {
+                if (((ii+KTMOVi[next]) >= 0) && ((ii +KTMOVi[next]) < 8) && ((jj + KTMOVj[next]) >= 0) && ((jj + KTMOVj[next]) < 8))
+                {
+                    if (board[(ii + KTMOVi[next]), (jj + KTMOVj[next])] == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public bool randMoveK(int[,] board)
+        {
+            int move;
+            int tempi, tempj;
+            for (int i = 0; i < 8; i++)
+            {
+                mov1[i] = 0;
+            }
+
+            //assigns moves that place the knight outside the board or onto a previously visited space -1 indicating invalid
+            for (int i = 0; i < 8; i++)
+            {
+                tempi = loci + KTMOVi[i];
+                tempj = locj + KTMOVj[i];
+                if (tempi < 0 || tempi >= 8 || tempj < 0 || tempj >= 8 || board[tempi, tempj] != 0)
+                {
+                    mov1[i] = -1;
+                }
+            }
+
+            //if all possible moves are invalid, return false to exit
+            if (Array.IndexOf(mov1, 0) == -1)
+                return false;
+            else
+            {
+                do
+                {
+                   move = rnd.Next(0, 8);
+                } while (mov1[move] != 0);
+                loci = loci + KTMOVi[move];
+                locj = locj + KTMOVj[move];
+                board[loci, locj] = order++;
+                return true;
+            }
+
+        }
+        //moves the knight to the position with least number of exits 
+        public bool moveK(int[,] board)
+        {
+            int low = 999;
+            int lowind = 999;
+
+            // finds the lowest non-zero value in the array mov1 and 
+            for (int i = 0; i < 8; i++)
+            {
+                if ((mov1[i] > 1) && (mov1[i] <= low))
+                {
+                    lowind = i;
+                    low = mov1[i];
+                }
+            }
+            //ensures that we travel to the final open position
+            if(lowind==999)
+            {
+                for(int i = 0; i< 8; i++)
+                {
+                    if(mov1[i]==1)
+                    {
+                        lowind = i;
+                    }
+                }
+            }
+
+            
+            // if there are no possible movement options left, return false, exiting the loop
+            if(lowind ==999)   
+            {
+                return false;
+            }
+
+            //moves the knight, incrementing order to put into the next position
+            loci = loci + KTMOVi[lowind];
+            locj = locj + KTMOVj[lowind];
+            board[loci, locj] = order;
+            order++;
+            
+            //resets the mov1 array
+            for (int i = 0; i< 8; i++)
+            {
+                mov1[i] = 0;
+            }
+            return true;
+        }
+        ~Knight() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        //@author: Austin Wang 11/14/2017
+        //Performs the Knight's Tour using both Heuristic and Brute Force algorithms. 
+        //Heuristic algorithm can be improved by maintaining heuristic board values as opposed to re-calculating them for each move
+        //
+        static void Main(string[] args)
+        {
+            
+            //2 dimensional array that instantiates the chessboard
+            // row 0 and column 0 are unused
+            int[,] BOARD = new int[8, 8];
+            int starti, startj;
+
+            Console.Write("Enter a starting row(0-7): ");
+            startj = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a starting column(0-7): ");
+            starti = Convert.ToInt32(Console.ReadLine());
+
+            Knight white = new Knight(starti, startj);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+
+            runHeuristic(white, BOARD);
+            //runBruteForce(white, BOARD, starti, startj);
+
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("\nThe Knight's Tour finished in " + elapsedMs + " milliseconds");
+            Console.ReadKey();
+            
+        }
+        static void runHeuristic(Knight K, int[,] BOARD){
+            bool complete = false;
+            clearBoard(BOARD);
+            BOARD[K.Loci, K.Locj] = 1;
+
+            do
+            {
+                K.checkmov(BOARD);
+                complete = K.moveK(BOARD);
+            } while (complete);
+
+            printArr(BOARD);
+        }
+        static void runBruteForce(Knight K, int[,] BOARD, int i, int j)
+        {
+            int count = 0;
+            int[] record = new int[64];
+            bool complete = false;
+
+            do
+            {
+                //resets the knight's starting location and order to the one given by the user
+                K.reset(i, j);
+                clearBoard(BOARD);
+
+                BOARD[K.Loci, K.Locj] = 1;
+
+                do
+                {
+                    complete = K.randMoveK(BOARD);
+                } while (complete);
+
+                record[K.order - 2] += 1;
+                count++;
+            } while (K.order != 65);
+
+            printArr(BOARD);
+            Console.WriteLine("\nIt took " + count + " tours to find a complete path using Brute Force(random number generation)\n");
+            printArr(record);
+
+       
+        }
+        static void clearBoard(int[,] Arr)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Arr[i, j] = 0;
+                }
+            }
+        }
+        static void printArr(int[,] Arr)
+        {
+            Console.WriteLine();
+            for(int j = 0; j<8; j++)
+            {
+                for(int i = 0; i<8; i++)
+                {
+                    Console.Write(Arr[i, j]);
+                    Console.Write("\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void printArr(int[] Arr)
+        {
+            Console.WriteLine("The number of attempts that ended at n moves, in order, are: ");
+            for (int i = 0; i < 64; i++)
+            {
+                Console.Write(Arr[i]);
+                Console.Write(", ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
